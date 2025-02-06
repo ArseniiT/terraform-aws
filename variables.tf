20c503554b9b30761169c97225f2143b8ca19af5
ArseniiT <arseniit@gmail.com>
2025-02-06 12:19:40 +0100

    Add Terraform variables for MySQL

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# login and password for mysql database
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
