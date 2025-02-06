0d3cd11a444552e5bc8cb32cd116089d9b60ac43
ArseniiT <arseniit@gmail.com>
2025-02-06 21:24:08 +0100

    Add egress rule to allow all outbound traffic in security group of MySQL

--- securitygroup.tf
+++ securitygroup.tf
@@ -42,4 +42,12 @@ resource "aws_security_group" "db_sg" {
     security_groups = [aws_security_group.apache_php_sg.id]
   }
 
+  # Allow outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
